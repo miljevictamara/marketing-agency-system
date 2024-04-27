3970d233c0a919fd3786b7939a3f2d9446e3101d
Tamara Miljevic <miljevictamara@gmail.com>
2024-04-27 16:05:02 +0200

    back-end

---
+++ back-end/marketing-acency/marketing-acency/src/main/java/com/bsep/marketingacency/MarketingAcencyApplication.java
@@ -0,0 +1,13 @@
+package com.bsep.marketingacency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingAcencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingAcencyApplication.class, args);
+	}
+
+}
---
+++ back-end/marketing-acency/marketing-acency/src/test/java/com/bsep/marketingacency/MarketingAcencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsep.marketingacency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingAcencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
