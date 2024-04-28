181cabadadfbd0d6cd72bad3f09b74942cfe88ac
Tamara Miljevic <miljevictamara@gmail.com>
2024-04-28 14:00:03 +0200

    feat: navbar

--- front-end/marketing-acency/src/app/app.module.ts
+++ front-end/marketing-acency/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './feature-modules/layout/navbar/navbar.component';
+import { LayoutModule } from './feature-modules/layout/layout.module';
+import { FooterComponent } from './feature-modules/layout/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
