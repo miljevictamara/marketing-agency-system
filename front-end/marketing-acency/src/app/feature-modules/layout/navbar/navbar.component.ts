7d504861e021d4c876cc5cf9af7b8569fa650932
Nina <nina.batranovic3@gmail.com>
2024-05-14 11:29:38 +0200

    feat: nav-bar permissions

--- front-end/marketing-acency/src/app/feature-modules/layout/navbar/navbar.component.ts
+++ front-end/marketing-acency/src/app/feature-modules/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { Role } from 'src/app/infrastructure/auth/model/role.model';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
 
 @Component({
@@ -10,7 +11,7 @@ import { User } from 'src/app/infrastructure/auth/model/user.model';
 })
 export class NavbarComponent {
   user : User | undefined
-
+  role : String | undefined
   constructor(private router: Router, private authService: AuthService){
     
   }
@@ -18,9 +19,22 @@ export class NavbarComponent {
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
       this.user = user;
+      this.hasClientRole()
     });
   }
 
+  hasClientRole(): boolean {
+   return (this.user?.roles[0]?.toString() === 'ROLE_CLIENT') 
+  }
+
+  hasEmployeeRole(): boolean {
+    return (this.user?.roles[0]?.toString() === 'ROLE_EMPLOYEE') 
+  }
+
+  hasAdminRole(): boolean {
+    return (this.user?.roles[0]?.toString() === 'ROLE_ADMIN') 
+
+  }
   logout():void {
     this.authService.logout();
   }
@@ -31,5 +45,6 @@ export class NavbarComponent {
   navigateTo(path: string): void {
     this.router.navigate([path]);
   }
+
 
 }
