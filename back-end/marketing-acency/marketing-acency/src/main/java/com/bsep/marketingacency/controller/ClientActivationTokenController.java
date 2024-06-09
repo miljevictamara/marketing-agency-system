d3e00497d09db12f1598dfd81638cee1e52e643c
Tamara Miljevic <miljevictamara@gmail.com>
2024-06-09 21:11:32 +0200

    feat: logs added for ClientActivationController

--- back-end/marketing-acency/marketing-acency/src/main/java/com/bsep/marketingacency/controller/ClientActivationTokenController.java
+++ back-end/marketing-acency/marketing-acency/src/main/java/com/bsep/marketingacency/controller/ClientActivationTokenController.java
@@ -4,6 +4,8 @@ import com.bsep.marketingacency.model.ClientActivationToken;
 import com.bsep.marketingacency.model.User;
 import com.bsep.marketingacency.service.ClientActivationTokenService;
 import com.bsep.marketingacency.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,16 +22,36 @@ public class ClientActivationTokenController {
     @Autowired
     private EmailService emailService;
 
+    private Logger logger =  LoggerFactory.getLogger(ClientActivationTokenController.class);
+
+//    @GetMapping("/{tokenId}/{hmac}")
+//    public ResponseEntity<User> findUser(@PathVariable UUID tokenId, @PathVariable String hmac) {
+//        User user = clientActivationTokenService.findUser(tokenId);
+//        Boolean isTokenUsed = clientActivationTokenService.checkIfUsed(tokenId);
+//        Boolean isHmacMatches = emailService.verifyHmac("https://localhost:4200/activation/" + tokenId, hmac);
+//        if (user != null && !isTokenUsed && isHmacMatches) {
+//            return ResponseEntity.ok(user);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+//        }
+//    }
+
     @GetMapping("/{tokenId}/{hmac}")
     public ResponseEntity<User> findUser(@PathVariable UUID tokenId, @PathVariable String hmac) {
-        User user = clientActivationTokenService.findUser(tokenId);
-        Boolean isTokenUsed = clientActivationTokenService.checkIfUsed(tokenId);
-        Boolean isHmacMatches = emailService.verifyHmac("https://localhost:4200/activation/" + tokenId, hmac);
-        if (user != null && !isTokenUsed && isHmacMatches) {
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        try {
+            User user = clientActivationTokenService.findUser(tokenId);
+            Boolean isTokenUsed = clientActivationTokenService.checkIfUsed(tokenId);
+            Boolean isHmacMatches = emailService.verifyHmac("https://localhost:4200/activation/" + tokenId, hmac);
+            if (user != null && !isTokenUsed && isHmacMatches) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+        } catch (Exception e) {
+            logger.error("Error finding user with token ID {}: {}", tokenId, e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
 
 }
--- back-end/marketing-acency/marketing-acency/src/main/java/com/bsep/marketingacency/service/ClientActivationTokenService.java
+++ back-end/marketing-acency/marketing-acency/src/main/java/com/bsep/marketingacency/service/ClientActivationTokenService.java
@@ -1,10 +1,14 @@
 package com.bsep.marketingacency.service;
 
+import com.bsep.marketingacency.controller.ClientActivationTokenController;
 import com.bsep.marketingacency.model.Client;
 import com.bsep.marketingacency.model.ClientActivationToken;
 import com.bsep.marketingacency.model.User;
 import com.bsep.marketingacency.repository.ClientActivationTokenRepository;
+import com.bsep.marketingacency.util.HashUtil;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,31 +24,83 @@ public class ClientActivationTokenService {
     private UserService userService;
     @Autowired
     private  ClientService clientService;
+    private Logger logger =  LoggerFactory.getLogger(ClientActivationTokenService.class);
+
     public ClientActivationToken save(ClientActivationToken token){
         return clientActivationTokenRepository.save(token);
     }
 
+//    public User findUser(UUID tokenId){
+//        ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElseGet(null);
+//        Client client = clientService.findById(token.getUser().getId());
+//        if(isExpired(token)){
+//            //userService.delete(token.getUser());
+//            //clientService.delete(client);
+//            return  null;
+//        }
+//        User user = token.getUser();
+//        return user;
+//    }
+
     public User findUser(UUID tokenId){
-        ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElseGet(null);
-        Client client = clientService.findById(token.getUser().getId());
-        if(isExpired(token)){
-            //userService.delete(token.getUser());
-            //clientService.delete(client);
-            return  null;
+        try {
+            ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElse(null);
+            if (token == null) {
+                logger.error("Client activation token with ID {} not found.", HashUtil.hash(tokenId.toString()));
+                return null;
+            }
+
+            if (isExpired(token)) {
+                logger.warn("Client activation token with ID {} has expired.", HashUtil.hash(tokenId.toString()));
+                return null;
+            }
+
+            Client client = clientService.findById(token.getUser().getId());
+            if (client == null) {
+                logger.error("Client not found for token with ID {}.", HashUtil.hash(tokenId.toString()));
+                return null;
+            }
+
+            User user = token.getUser();
+            return user;
+        } catch (Exception e) {
+            logger.error("Error finding user with token ID {}: {}", HashUtil.hash(tokenId.toString()), e.getMessage());
+            return null;
         }
-        User user = token.getUser();
-        return user;
     }
 
+
+//    public Boolean checkIfUsed(UUID tokenId){
+//        ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElseGet(null);
+//        if(!token.getIsUsed()){
+//            token.setIsUsed(true);
+//            save(token);
+//            return false;
+//        }
+//        return  true;
+//    }
+
     public Boolean checkIfUsed(UUID tokenId){
-        ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElseGet(null);
-        if(!token.getIsUsed()){
-            token.setIsUsed(true);
-            save(token);
-            return false;
+        try {
+            ClientActivationToken token = clientActivationTokenRepository.findById(tokenId).orElse(null);
+            if (token == null) {
+                logger.error("Client activation token with ID {} not found.", HashUtil.hash(tokenId.toString()));
+                return null;
+            }
+
+            if (!token.getIsUsed()) {
+                token.setIsUsed(true);
+                save(token);
+                return false;
+            }
+
+            return true;
+        } catch (Exception e) {
+            logger.error("Error checking if token with ID {} is used: {}", HashUtil.hash(tokenId.toString()), e.getMessage());
+            return null;
         }
-        return  true;
     }
+
 
     public boolean isExpired(ClientActivationToken token){
         Date currentTime = new Date();
